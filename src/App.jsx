7c472007cca25ef5e352f70b338419fbd75755ee
Regi Adi <regiadi.priyanto@gmail.com>
2019-05-03 09:43:20 +0700

    Add axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from 'react'
 
-function App() {
-    return (
-        <div className="App">
-            <h1>Stock Market Update</h1>    
-        </div>
-    );
+import axios from 'axios'
+
+export default class App extends React.Component {
+    state = {
+        price: 0
+    }
+    
+    componentDidMount() {
+        axios.get('https://cloud.iexapis.com/beta/stock/aapl/price?token=pk_ca88eb0f892e4e11bfcef431cd53cc99')
+            .then(response => {
+                console.log(response)
+                const price = response.data
+                this.setState({price})
+            })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Stock Market Update</h1>    
+                <p>{this.state.price}</p>
+            </div>
+        )
+    }
 }
-
-export default App;
