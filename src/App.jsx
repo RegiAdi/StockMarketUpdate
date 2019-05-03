7e2515895c8596cbbc383e8d5dcba3bb53b56d38
Regi Adi <regiadi.priyanto@gmail.com>
2019-05-03 11:07:12 +0700

    Get stock price data using axios and react hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,20 @@
-import React from 'react'
-
+import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
-export default class App extends React.Component {
-    state = {
-        price: 0
-    }
-    
-    componentDidMount() {
-        axios.get('https://cloud.iexapis.com/beta/stock/aapl/price?token=pk_ca88eb0f892e4e11bfcef431cd53cc99')
-            .then(response => {
-                console.log(response)
-                const price = response.data
-                this.setState({price})
-            })
-    }
+export default function App() {
+    const [price, setPrice] = useState(0)
 
-    render() {
-        return (
-            <div className="App">
-                <h1>Stock Market Update</h1>    
-                <p>{this.state.price}</p>
-            </div>
-        )
-    }
+    useEffect(() => {
+        axios.get('https://sandbox.iexapis.com/beta/stock/aapl/price?token=Tpk_2596d5035d964c71a66255c363ec1e3d')
+        .then(response => {
+            console.log(response)
+            setPrice(response.data)
+        })
+    }, [])
+    return ( 
+        <div className="App">
+            <h1>Stock Market Update</h1>    
+            <p>{price}</p>
+        </div>
+    )
 }
