62ba24c15fafd3717a84e61aa320ffea97a44a08
Regi Adi <regiadi.priyanto@gmail.com>
2019-05-05 11:22:25 +0700

    Add buttons in App.jsx that can be used to choose api endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Price from './Price'
 import BalanceSheet from './BalanceSheet'
 import BatchRequests from './BatchRequests'
 import Book from './Book'
 
 export default function App() {
+    const [activeEndpoint, setActiveEndpoint] = useState('price')
+
     return (
         <div className="App">
             <h1>Stock Market Update</h1>
 
-            <Book />
-            <BatchRequests />
-            <Price />
-            <BalanceSheet />
+            <p>
+                <button onClick={() => setActiveEndpoint('price')}>
+                    Price
+                </button>
+
+                <button onClick={() => setActiveEndpoint('balanceSheet')}>
+                    Balance Sheet
+                </button>
+
+                <button onClick={() => setActiveEndpoint('book')}>
+                    Book
+                </button>
+
+                <button onClick={() => setActiveEndpoint('batchRequests')}>
+                    Batch Requests
+                </button>
+            </p>
+
+            {activeEndpoint === 'price' && <Price />}
+            {activeEndpoint === 'balanceSheet' && <BalanceSheet />}
+            {activeEndpoint === 'book' && <Book />}
+            {activeEndpoint === 'batchRequests' && <BatchRequests />}
+
         </div>
     )
 }
